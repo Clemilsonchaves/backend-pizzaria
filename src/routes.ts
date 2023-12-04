fb1dc1ccea465811ff8707117bf36ca4ed67ad9f
clemilson chaves <cle_milson_silva@hotmail.com>
2023-12-04 04:29:19 -0300

    create category and list category

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,9 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+
 import { isAuthenticated  } from "./middlewares/isAuthenticated";
 
 const router = Router();
@@ -13,5 +16,9 @@ const router = Router();
 router.post('/users', new CreateUserController().handle) // criar um usuário
 router.post('/session', new AuthUserController().handle ) // logar um usuário 
 router.get('/me', isAuthenticated, new DetailUserController().handle) // Detalhes do usuário
+
+// -- ROTAS CATEGORY 
+router.post('/category', isAuthenticated, new CreateCategoryController().handle) // Criando uma categoria
+router.get('/category', isAuthenticated, new ListCategoryController().handle)    // Listando uma categoria
 
 export { router };
