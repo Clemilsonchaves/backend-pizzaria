fb50fee9455dcb3b7f34a9deedb3420552d3f83f
clemilson chaves <cle_milson_silva@hotmail.com>
2023-12-23 09:54:06 -0300

    finish order

--- src/routes.ts
+++ src/routes.ts
@@ -20,6 +20,7 @@ import { SendOrderController } from "./controllers/order/SendOrderController";
 
 import { ListOrdersController } from "./controllers/order/ListOrdersController";
 import { DetailOrderController }  from "./controllers/order/DetailOrderController"; 
+import { FinishOrderController } from "./controllers/order/FinishOrderController";
 
 import { isAuthenticated  } from "./middlewares/isAuthenticated";
 
@@ -55,6 +56,8 @@ router.put('/order/send', isAuthenticated, new SendOrderController().handle) // 
 
 router.get('/orders', isAuthenticated, new ListOrdersController().handle) // Chamando  os ultimos pedidos
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle) //  Ver os detalhes do pedido
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle) // Finalizanndo a order
 
 export { router }; 
 
